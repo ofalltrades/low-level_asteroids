b02fe7a9391fc381a95cd9348ce519268fcf43e9
Jake Johnson <kognizant.kog@gmail.com>
2016-10-30 06:02:13 -0400

    break defines and typedefs out into headers

---
+++ src/commonplace.h
@@ -0,0 +1,24 @@
+#ifndef COMMONPLACE
+#define COMMONPLACE
+
+#define internal      static
+#define local_persist static
+#define global_var    static
+#define readonly      const
+
+typedef   char                i8;
+typedef   short               i6;
+typedef   int                 i32;
+typedef   double              i64;
+
+typedef   unsigned char       u8;
+typedef   unsigned short      u16;
+typedef   unsigned int        u32;
+typedef   unsigned long long  u64;
+
+typedef   float               r32;
+typedef   double              r64;
+
+typedef   int                 bool;
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -4,25 +4,10 @@
  */
 
 #include <SDL2/SDL.h>
-#include <math.h>
+#include <math.h>         // Remove after implementing sine
+#include "commonplace.h"
 
-#define internal      static
-#define local_persist static
-#define global_var    static
-#define readonly      const
-
-typedef   char                int8;
-typedef   short               int16;
-typedef   int                 int32;
-typedef   double              int64;
-
-typedef   unsigned char       uint8;
-typedef   unsigned short      uint16;
-typedef   unsigned int        uint32;
-typedef   unsigned long long  uint64;
-
-typedef   float               real32;
-typedef   double              real64;
+#define PI32 = 3.14159265359
 
 #define BLACK     0b00000000'00000000'00000000'00000000
 #define WHITE     0b00000000'11111111'11111111'11111111
@@ -36,57 +21,57 @@ typedef   double              real64;
 //
 // Note: The above measurements should only be relied on with respect
 //       to this initial state
-global_var real32 shipScalars[10] = {  0.0f,  15.0f,    // nose
-                                      -9.0f, -15.0f,    // left wing tip
-                                       9.0f, -15.0f,    // right wing tip
-                                      -6.0f, -10.0f,    // left base scalar
-                                       6.0f, -10.0f };  // right base scalar
+global_var r32 shipScalars[10] = {  0.0f,  15.0f,    // nose
+                                   -9.0f, -15.0f,    // left wing tip
+                                    9.0f, -15.0f,    // right wing tip
+                                   -6.0f, -10.0f,    // left base scalar
+                                    6.0f, -10.0f };  // right base scalar
 
 global_var struct {
-  const int32 width              = 640;
-  const int32 height             = 480;
-  const int32 bufferSize         = width * height;
-  const int32 bufferSizeInBytes  = bufferSize * 4;
+  const i32 width              = 640;
+  const i32 height             = 480;
+  const i32 bufferSize         = width * height;
+  const i32 bufferSizeInBytes  = bufferSize * 4;
 } screenData;
 
 
 // I'm using calloc here rather than mmap or vm_allocate because
 //   it is only called once and is more portable. (Is this true?)
-global_var uint32 *
-screenBuffer_ptr = ( uint32 * )calloc( ( screenData.width * screenData.height ),
-                                       screenData.bufferSizeInBytes );
+global_var u32 *
+screenBuffer_ptr = ( u32 * )calloc( ( screenData.width * screenData.height ),
+                                      screenData.bufferSizeInBytes );
 
 internal void
-convertToScreenCoordinates( int32 x, int32 y, real32 * scalars ) {
-  for ( int32 i = 0; i < 10; i += 2 ) {
+convertToScreenCoordinates( i32 x, i32 y, r32 * scalars ) {
+  for ( i32 i = 0; i < 10; i += 2 ) {
     scalars[i + 1] =  -scalars[i + 1]; // Invert Y
     scalars[i]     += x;
     scalars[i + 1] -= y;
   }
 }
 
-internal void plotPixel( int32    x,
-                         int32    y,
-                         uint32 * pixels_ptr,
-                         uint32   color ) {
-  int32    posInMemory   = ( screenData.width * y ) + x;
-  uint32 * pixel         = pixels_ptr + posInMemory;
+internal void plotPixel( i32   x,
+                         i32   y,
+                         u32 * pixels_ptr,
+                         u32   color ) {
+  i32   posInMemory   = ( screenData.width * y ) + x;
+  u32 * pixel         = pixels_ptr + posInMemory;
 
   *pixel = color;
 }
 
-internal void drawLine( int32    x1,
-                        int32    y1,
-                        int32    x2,
-                        int32    y2,
-                        uint32 * pixels_ptr,
-                        uint32   color ) {
-  int32 dx  = abs( x2 - x1 ),
-        sx  = x1 < x2 ? 1 : -1,           // X Direction (sign of X)
-        dy  = abs( y2 - y1 ),
-        sy  = y1 < y2 ? 1 : -1,           // Y Direction (sign of Y)
-        err = ( dx > dy ? dx : -dy ) / 2,
-        prevErr;
+internal void drawLine( i32    x1,
+                        i32    y1,
+                        i32    x2,
+                        i32    y2,
+                        u32 *  pixels_ptr,
+                        u32    color ) {
+  i32 dx  = abs( x2 - x1 );
+  i32 sx  = x1 < x2 ? 1 : -1;           // X Direction (sign of X)
+  i32 dy  = abs( y2 - y1 );
+  i32 sy  = y1 < y2 ? 1 : -1;           // Y Direction (sign of Y)
+  i32 err = ( dx > dy ? dx : -dy ) / 2;
+  i32 prevErr;
 
   // Plot first pixel manually.
   plotPixel( x1, y1, pixels_ptr, color );
@@ -108,11 +93,11 @@ internal void drawLine( int32    x1,
 }
 
 // 'scalars' is of the form [X0, Y0, X1, Y1, ... Xn, Yn].
-internal void rotateScalarsLeft( real32 * scalars, real32 rad ) {
-  real32 prevX;
-  int32  yIdx;
+internal void rotateScalarsLeft( r32 * scalars, r32 rad ) {
+  r32 prevX;
+  i32 yIdx;
 
-  for ( int32 x = 0; x < 10; x += 2 ) {
+  for ( i32 x = 0; x < 10; x += 2 ) {
     prevX = scalars[x];
     yIdx = x + 1;
 
@@ -121,36 +106,40 @@ internal void rotateScalarsLeft( real32 * scalars, real32 rad ) {
   }
 }
 
-internal void drawShip( int32 x, int32 y, uint32 * pixels_ptr, uint32 color ) {
+internal void drawShip( i32 x, i32 y, u32 * pixels_ptr, u32 color ) {
   // Left side
-  drawLine( x + ( int32 )shipScalars[0],       // nose X pos
-            y - ( int32 )shipScalars[1],       // nose Y pos
-            x + ( int32 )shipScalars[2],       // left wing tip X pos
-            y - ( int32 )shipScalars[3],       // left wing tip Y pos
+  drawLine( x + ( i32 )shipScalars[0],       // nose X pos
+            y - ( i32 )shipScalars[1],       // nose Y pos
+            x + ( i32 )shipScalars[2],       // left wing tip X pos
+            y - ( i32 )shipScalars[3],       // left wing tip Y pos
             pixels_ptr,
             color );
 
   // Right side
-  drawLine( x + ( int32 )shipScalars[0],       // nose X pos
-            y - ( int32 )shipScalars[1],       // nose Y pos
-            x + ( int32 )shipScalars[4],       // right wing tip X pos
-            y - ( int32 )shipScalars[5],       // right wing tip Y pos
+  drawLine( x + ( i32 )shipScalars[0],       // nose X pos
+            y - ( i32 )shipScalars[1],       // nose Y pos
+            x + ( i32 )shipScalars[4],       // right wing tip X pos
+            y - ( i32 )shipScalars[5],       // right wing tip Y pos
             pixels_ptr,
             color );
 
   // Base
   // Use absolute value here to translate Cartesian coordinates
   //   into right-handed Cartesian.
-  drawLine( x + ( int32 )shipScalars[6],           // left base X pos
-            y + ( int32 )abs( shipScalars[7] ),    // left base Y pos
-            x + ( int32 )shipScalars[8],           // right base X pos
-            y + ( int32 )abs( shipScalars[9]),    // right base Y pos
+  drawLine( x + ( i32 )shipScalars[6],           // left base X pos
+            y + ( i32 )abs( shipScalars[7] ),    // left base Y pos
+            x + ( i32 )shipScalars[8],           // right base X pos
+            y + ( i32 )abs( shipScalars[9]),     // right base Y pos
             pixels_ptr,
             color );
 }
 
-int32 main() {
-  real32 rad = 0.7f;
+void gameUpdateAndRender() {
+
+}
+
+i32 main() {
+  r32 rad = 0.7f;
 
   //convertToScreenCoordinates( 100, 100, shipScalars );
 
@@ -158,7 +147,7 @@ int32 main() {
   drawShip( 50, 50, screenBuffer_ptr, WHITE );
   drawShip( 50, 50, screenBuffer_ptr, MAGENTA );
 
-  for ( int32 i = 0; i < 1; ++i ) {
+  for ( i32 i = 0; i < 1; ++i ) {
     rotateScalarsLeft( shipScalars, rad );
     drawShip( 50, 50, screenBuffer_ptr, WHITE );
     drawShip( 50, 50, screenBuffer_ptr, BLACK );
@@ -183,7 +172,7 @@ int32 main() {
   screen = SDL_GetWindowSurface( window );
 
   // Fill screen with black pixels.
-  uint32 * pixels_ptr = (uint32 *)screen->pixels;
+  u32 * pixels_ptr = (u32 *)screen->pixels;
   memcpy( pixels_ptr, screenBuffer_ptr, screenData.bufferSizeInBytes );
 
   SDL_UpdateWindowSurface( window );
